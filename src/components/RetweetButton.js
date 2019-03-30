9bb2a6438523f6964f6cdd3a54d0c6e385767cba
khwilo <khwilowatai@gmail.com>
2019-03-30 11:57:13 +0300

    Allow only integer values for the count property

--- src/components/RetweetButton.js
+++ src/components/RetweetButton.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Count = ({ count }) => {
     if (count > 0) {
@@ -24,4 +25,7 @@ const RetweetButton = ({ count }) => {
     );
 };
 
+RetweetButton.propTypes = {
+    count: PropTypes.number
+};
 export default RetweetButton;
